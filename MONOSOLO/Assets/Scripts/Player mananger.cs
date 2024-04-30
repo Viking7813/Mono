aff263818d16803277e09301e74d0be9bf8e973b
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2024-04-30 11:59:50 +0200

    fucking cancer helvete

--- MONOSOLO/Assets/Scripts/Player mananger.cs
+++ MONOSOLO/Assets/Scripts/Player mananger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playermananger : MonoBehaviour
@@ -8,25 +9,39 @@ public class Playermananger : MonoBehaviour
 
     public List<GameObject> tiles;
 
-    public Dice dice; 
+    public Dice dice;
 
+    public Player p;
     // Start is called before the first frame update
     void Start()
     {
+        int i = 0;
         foreach (GameObject player in playerlist)
         {
             player.transform.position = tiles[0].transform.position;
+            i++;
         }
+
+        //p = playerlist[0].GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        foreach (GameObject obj in tiles)
+        /*foreach (GameObject players in playerlist)
         {
-
-        }
+            //p = players.GetComponent<Player>();
+            
+            if (p.id == 1)
+            {
+                p.index += dice.nummer;
+            }
+        }*/
+        /*if (p.id == 1)
+            {
+                p.index += dice.nummer;
+            }*/
+        //p.index = dice.nummer;
+        // kolla om id stämmer överräns med player id. Försök nå player scripten i varje player i playerlist.
     }
 }
